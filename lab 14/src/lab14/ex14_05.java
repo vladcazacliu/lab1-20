2a5249b7a464855f48bd2297f293c6d75c9d56de
vladcazacliu <90508495+vladcazacliu@users.noreply.github.com>
2021-12-03 19:57:47 +0200

    Update ex14_05.java

--- lab 14/src/lab14/ex14_05.java
+++ lab 14/src/lab14/ex14_05.java
@@ -12,17 +12,17 @@ public class ex14_05 {
 
         String[] sentences = new String[20];
 
-        // cria 20 frases
+      
         for (int i = 0; i < 20; i++) {
             String art = article[random.nextInt(5)];
-            // deixa a primeira letra maiúscula
+           
             art = art.replaceFirst(art.substring(0, 1), art.substring(0, 1).toUpperCase());
 
             sentences[i] = String.format("%s %s %s %s %s %s.", art, noun[random.nextInt(5)], verb[random.nextInt(5)],
                     prepositon[random.nextInt(5)], article[random.nextInt(5)], noun[random.nextInt(5)]);
         }
 
-        // exibe as frases
+       
         for (int i = 0; i < sentences.length; i++)
             System.out.printf("%d° Frase: %s%n", i + 1, sentences[i]);
 
